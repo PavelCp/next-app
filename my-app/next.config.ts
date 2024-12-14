fb648844ecc89a533b050a059ad684a5e344448b
Pavel C <pavelcapet@gmail.com>
2024-12-14 19:14:36 +0200

    changed README.md

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 'https://github.com/PavelCp/next-app'
+  output: "export",
+  basePath: "/next-app",
 };
 
 export default nextConfig;
