460bd90896ca051ef32f001ef20c087ccabb908e
Pavel C <pavelcapet@gmail.com>
2024-12-14 20:52:19 +0200

    fix typo in publish.yml

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   // 'https://github.com/PavelCp/next-app'
   output: "export",
   basePath: "/next-app",
-  // reactStrictMode: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
